315c47d6864bae36adad66ff0dbeb23426f70322
Tyler Byers <tbyers@risklens.com>
2018-08-15 16:19:02 -0700

    wrote setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open('requirements.txt', 'r') as f:
+	install_requires = f.read().splitlines()
+
+setup(
+	name='verispy',
+	version='0.1.0',
+	description='Parses VCDB json data into a Pandas DataFrame and provides useful analytic functions',
+	author='Tyler Byers',
+	author_email='tbyers@risklens.com',
+	packages=['verispy', 'versipy.tests'],
+	license='LICENSE.txt',
+	long_description=open('README.txt').read(),
+	install_requires=install_requires,
+	include_package_data=True
+)
