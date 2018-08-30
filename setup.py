2e344953380e1d2f0b4857d84fd488fbdc71ef70
Tyler Byers <tbyers@risklens.com>
2018-08-29 17:25:21 -0700

    long description

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 with open('requirements.txt', 'r') as f:
 	install_requires = f.read().splitlines()
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
 	name='verispy',
 	version='0.1.0',
@@ -11,7 +14,9 @@ setup(
 	author_email='tbyers@risklens.com',
 	packages=['verispy', 'verispy.tests'],
 	license='LICENSE.txt',
-	#long_description=open('README.txt').read(),
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	#url='https://fillinwhenIhaveahome'
 	install_requires=install_requires,
 	include_package_data=True
 )
