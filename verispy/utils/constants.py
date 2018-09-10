1988c45be4a0622fc5ef5275b6676d3ba6796a4e
Tyler Byers <tbyers@risklens.com>
2018-09-10 11:31:55 -0700

    variety/loss enumeration for impact.loss.<amount/variety>

--- verispy/utils/constants.py
+++ verispy/utils/constants.py
@@ -1,5 +1,5 @@
 SCHEMA_URL = "https://raw.githubusercontent.com/vz-risk/veris/master/verisc-merged.json"
-VARIETY_AMT_ENUMS = ['asset.assets', 'attribute.confidentiality.data']
+VARIETY_AMT_ENUMS = ['asset.assets', 'attribute.confidentiality.data', 'impact.loss']
 VARIETY_AMT = ['variety', 'amount']
 ASSETMAP = {'S ' : 'Server', 'N ' : 'Network', 'U ' : 'User Dev', 'M ' : 'Media',
             'P ' : 'Person', 'T ' : 'Kiosk/Term', 'Un' : 'Unknown', 'E ' : 'Embedded'}
