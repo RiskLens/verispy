935df382515f926056b7c9b967db1349bf1093b7
Tyler Byers <tbyers@risklens.com>
2019-01-03 09:36:51 -0800

    MIT license specified in setup file

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ with open("README.md", "r") as fh:
 setup(
 	name='verispy',
 	version='0.1.0',
-	description='Parses VCDB json data into a Pandas DataFrame and provides useful analytic functions',
+	description='Parses VCDB json data into a Pandas DataFrame and provides summary functions and basic enumeration plotting.',
 	author='Tyler Byers',
 	author_email='tbyers@risklens.com',
 	packages=['verispy', 'verispy.tests'],
-	license='LICENSE.txt',
+	license='MIT',
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	#url='https://fillinwhenIhaveahome'
