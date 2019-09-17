ae1d5d703a60103e3f800da4da53fb741d2048db
Tyler Byers <tbyers@risklens.com>
2019-09-17 16:31:00 -0700

    bump version

--- verispy/__init__.py
+++ verispy/__init__.py
@@ -1,8 +1,8 @@
 # Package written by Tyler Byers at RiskLens
 # tbyers@risklens.com
 # Spokane, WA USA
-# Copyright 2018 RiskLens
+# Copyright 2018-2019 RiskLens
 
 from .veris import VERIS
 
-__version__ = '0.1'
+__version__ = '0.1.5'
